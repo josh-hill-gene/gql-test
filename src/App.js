03304463ad1c348b5fcd4096f9fc35007579b5b7
Josh Hill <hillj38@gene.com>
2021-08-31 16:34:52 -0500

    finished tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import { API } from 'aws-amplify';
+import { listRecipes } from './graphql/queries';
+import { createRecipe } from './graphql/mutations';
+import config from './aws-exports';
 import './App.css';
+import { onCreateRecipe } from './graphql/subscriptions';
 
 function App() {
+
+  useEffect(() => {
+    const pullData = async () => {
+      const data = await API.graphql({ query: listRecipes });
+      console.log('data', data);
+    };
+    pullData();
+
+    const subscription = API.graphql({
+      query: onCreateRecipe
+    }).subscribe({
+      next: recipeData => {
+        pullData();
+      },
+      error: (err) => {
+        console.log('err: ', err);
+      }
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  const createNewRecipe = async () => {
+    const name = prompt('name your recipe friend');
+    const newRecipe = await API.graphql({
+      query: createRecipe,
+      variables: { input: { name } }
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={createNewRecipe}>Create new recipe</button>
     </div>
   );
 }
